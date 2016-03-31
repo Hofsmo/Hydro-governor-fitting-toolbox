d6ef7c0dc6e439956c4f2dee8ee1afa1d095d720
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-03-31 16:01:59 +0200

    On branch issue#3 #3  Changes to be committed: 	modified:   doComparison.m         Removed the scaling of the transfer to per unit. This should in         the future be done by the function calling doComparion

--- doComparison.m
+++ doComparison.m
@@ -23,7 +23,7 @@ if nargin < 4
     legends = 'show';
 end
 if iscell(results)
-    temp=cellfun(@(x) x*50/320, results,'UniformOutput', false);
+    temp=cellfun(@(x) x, results,'UniformOutput', false);
     bode(temp{:})
     legend(legends)
     grid on
@@ -61,7 +61,7 @@ elseif isstruct(results)
         doComparison(sys.(names{i}).sys,temp,names{i},{'VF','ARX'});
     end
 else
-    bode(results*50*320)
+    bode(results)
     if toLaTeX
         saveLaTeX ('Bode', name,figureSize)
     end
