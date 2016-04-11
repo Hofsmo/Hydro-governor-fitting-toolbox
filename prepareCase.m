8f4609e606887c106c5eaba2737850d743ae2376
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-04-11 13:16:33 +0200

    On branch issue8 #8  Changes to be committed: 	modified:   prepareCase.m     Close issue

--- prepareCase.m
+++ prepareCase.m
@@ -1,6 +1,26 @@
-function [p,f,t,ts] = prepareCase(filename, range)
+function [p,f,t,ts] = prepareCase(filename, range, window, factor)
+% PREPARECASE prepare the case for the identification
+% A function that prepares the signal for the identification
+% INPUT:
+%   filename: Namme of the file containing the case
+%   range: The range of values to use. The default is to use all
+%   window: The window size used for smoothing the signal
+%   factor: The factor used for decimation.
+% OUTPUT:
+%   p: The processed power
+%   f: The processed frequency
+%   t: The processed time
+%   ts: The time step
 
 [f,p] = readPMU(filename);
+
+if nargin < 4
+    factor = 0;
+end
+
+if nargin < 3
+    window = 0;
+end
 
 if nargin < 2
     f1 = f;
@@ -17,3 +37,17 @@ p = step(hDC3,p1);
 
 ts = 0.02;
 t=0:ts:((length(f1)-1)*ts);
+
+% Smooth the signals
+if window
+    p = smooth(p,window);
+    f = smooth(f,window);
+end
+
+% Decimate the signals
+if factor
+    p = decimate(p,factor);
+    f = decimate(f,factor);
+    t = decimate(t,factor);
+    ts = t(2)-t(1);
+end
