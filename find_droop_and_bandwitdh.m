6da9e5a4d494187805ffef790295c3e1c55b816b
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2017-01-27 17:33:08 +0100

    On branch GH-18/find_droop  Changes to be committed: 	modified:   find_droop_and_bandwitdh.m

--- find_droop_and_bandwitdh.m
+++ find_droop_and_bandwitdh.m
@@ -1,16 +1,17 @@
-function [ R, Rpu, bw ] = find_droop_and_bandwitdh(tf, Sb)
+function [ R, Rpu, bw ] = find_droop_and_bandwitdh(tf, Sb, fb)
 %UNTITLED function to find governor droop
 %   Caclulate the droop and bandwodth for turbine governors
 %
 %   INPUT:
 %       tf: Transfer function used to do the calculations
 %       Sb: Machine base power
+%       fb: Base frequency
 %   OUTPUT:
 %       R: The droop of the governor
 %       R_pu: Droop in per unit
 %       bw: The bandwidth of the governor
     gain = abs(tf.Numerator{1}(end)/tf.Denominator{1}(end));
     R = 1/gain;
-    Rpu = R/50*Sb/100;
+    Rpu = R/f_base*Sb;
     bw = bandwidth(tf)/(2*pi);
 end
