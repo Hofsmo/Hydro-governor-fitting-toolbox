135620be2eee1cd01c8eb2d3fc88abee69bca3e5
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2017-04-26 14:29:10 +0200

    On branch GH-25/ga  First function skeleton

---
+++ governor_fitness.m
@@ -0,0 +1,5 @@
+function fit = governor_fitness(x, var_names, model, f, p)
+%% GOVERNOR_FITNESS: Fitness function for governors
+% Function that runs a simulink model and calculate the fitness of the
+% parameters
+% INPUT
